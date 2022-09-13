06bb0ceeb833b83a70fdc55182e3a8b0746ccc62
Daniel Vazquez <dvazquez@greengates.edu.mx>
2022-09-12 20:04:25 -0500

    Initial code

---
+++ src/DependentCombos.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class DependentCombos {
+
+    private JPanel panel1;
+
+    private JLabel lblSport;
+    private JComboBox cbSport;
+    private JLabel lblDetail;
+    private JComboBox cbDetail;
+}
